c9fb232a5868812e7ebcfd084453fe632df1e9f1
chanrithroung2@gmail.com <chanrithroung2@gmail.com>
2024-11-28 00:06:08 +0700

    new updated

--- fetch_data.js
+++ fetch_data.js
@@ -3,7 +3,31 @@ const url = "https://fakestoreapi.com/products?limit=20";
 const root = document.getElementById('root');
 const search = document.getElementById("search");
 const btnSeach = document.querySelector("#btn-search");
+const form = document.getElementById("form")
 
+
+form.onsubmit = async (e) => {
+    const data = await getAPIdata();
+    const searchValue = search.value.toLowerCase();
+    console.log(searchValue);
+
+    const filterData = data.filter( e => e.title.toLowerCase().includes(searchValue))
+    const element = filterData.map( e => 
+        `<div class="col" data-aos="zoom-out-down">
+                <div class="card" style="width: 18rem;" >
+                    <img src="${e.image}"  class="thumbnail card-img-top" alt="thumbnial">
+                    <div class="card-body">
+                        <h5 class="card-title">${e.title}</h5>
+                        <p class="card-text badge bg-success text-light fs-4">${e.price}$</p>
+                        <p class="card-text text">${e.description}</p>
+                        <a href="#" class="btn btn-primary">Buy</a>
+                    </div>
+                </div>
+        </div>`
+     );
+    root.innerHTML = element;
+    e.preventDefault();
+}
 
 const getAPIdata = async () => {
     const response = await fetch(url);
@@ -11,7 +35,6 @@ const getAPIdata = async () => {
     return data;
 }
 
-// const data = await getAPIdata();
 
 
 btnSeach.addEventListener('click', async () => {
